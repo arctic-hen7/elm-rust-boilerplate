35a7d9b008f8d34e980580596ab010ddbc5794ad
arctic_hen7 <arctic_hen7@pm.me>
2021-04-18 08:35:14 +1000

    feat: 🚧 added basic mongodb connection logic

---
+++ api/src/load_env.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+// Loads all necessary environment files with `dotenv`
+pub fn load_env() -> Result<(), String> {
+    // Load the environment-specific environment variable files (checking whether we're in development/debug or production/release)
+    if cfg!(debug_assertions) {
+        load_env_file_if_present(".env.development")?; // Non-secret
+        load_env_file_if_present(".env.development.local")?; // Secret
+    } else {
+        load_env_file_if_present(".env.production")?; // Non-secret
+        load_env_file_if_present(".env.production.local")?; // Secret
+    }
+    // Load the files for all environments
+    load_env_file_if_present(".env")?; // Non-secret
+    load_env_file_if_present(".env.local")?; // Secret
+
+    Ok(())
+}
+
+// Loads the given environment file if it's present, otherwise does nothing (side-effect based function)
+fn load_env_file_if_present(filename: &str) -> Result<(), String> {
+    // Check if the file exists
+    if fs::metadata(filename).is_ok() {
+        let res = dotenv::from_filename(filename);
+        match res {
+            Ok(_) => return Ok(()),
+            Err(err) => return Err(format!(
+                "Error fetching environment file '{filename}', {:?}",
+                err,
+                filename=filename
+            ))
+        };
+    }
+    // If it doesn't exist, we don't worry about it, that's the point of this function
+    Ok(())
+}
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+mod load_env;
+
+use std::env;
+use mongodb::{
+    Client,
+    options::{ClientOptions, StreamAddress, Credential},
+    error::Error as MongoError,
+    bson::doc
+};
+use crate::load_env::load_env;
+
+fn get_client() -> Result<Client, MongoError> {
+    // Get all the necessary configuratio nfrom environment variables
+    let hostname = env::var("DB_HOSTNAME").expect("Environment variable 'DB_HOSTNAME' not present or invalid.");
+    let port = env::var("DB_PORT")
+        .expect("Environment variable 'DB_PORT' not present or invalid.")
+        .parse::<u16>() // Ports are not going to be larger than a 16-bit integer
+        .expect("Environment variable 'DB_PORT' should be a number, but it isn't.");
+    let username = env::var("DB_USERNAME").expect("Environment variable 'DB_USERNAME' not present or invalid.");
+    let password = env::var("DB_PASSWORD").expect("Environment variable 'DB_PASSWORD' not present or invalid.");
+
+    let options =
+        ClientOptions::builder()
+            .hosts(vec![
+                StreamAddress {
+                    hostname,
+                    port: Some(port),
+                }
+            ])
+            .credential(
+                Credential::builder()
+                    .username(username)
+                    .password(password)
+                    .build()
+            )
+            .build();
+
+    let client = Client::with_options(options)?;
+
+    Ok(client)
+}
+
+#[tokio::main]
+async fn main() {
+    load_env().expect("Error getting environment variables!");
+
+    let client = get_client().unwrap();
+    let db = client.database("test");
+    let users = db.collection("users");
+
+    let result = users.insert_one(doc! {
+        "name": "John Doe"
+    }, None).await.unwrap();
+
+    println!("{:?}", result)
 }
